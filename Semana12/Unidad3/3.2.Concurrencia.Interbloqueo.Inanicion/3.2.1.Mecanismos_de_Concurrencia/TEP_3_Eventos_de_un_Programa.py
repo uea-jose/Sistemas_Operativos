2a49b378874d438bb87aaf3d394d2c7467ee4e79
TEP?3?so <jv.avilar@uea.edu.ec>
2024-09-09 18:15:31 -0500

    v5

--- Semana12/Unidad3/3.2.Concurrencia.Interbloqueo.Inanicion/3.2.1.Mecanismos_de_Concurrencia/TEP_3_Eventos_de_un_Programa.py
+++ Semana12/Unidad3/3.2.Concurrencia.Interbloqueo.Inanicion/3.2.1.Mecanismos_de_Concurrencia/TEP_3_Eventos_de_un_Programa.py
@@ -6,20 +6,26 @@ evento = threading.Event()
 
 # Función que espera a que se active el evento
 def esperar_evento():
-    print("Esperando al evento...")  # Imprimimos un mensaje indicando que estamos esperando
+    # Imprimimos un mensaje indicando que estamos esperando
+    print("Esperando al evento...")
     evento.wait()  # Esperamos a que el evento se active
-    print("El evento ha sido activado!")  # Imprimimos un mensaje cuando el evento se activa
+    # Imprimimos un mensaje cuando el evento se activa
+    print("El evento ha sido activado!")
 
 # Función que activa el evento después de un cierto tiempo
 def activar_evento():
-    print("Esperando 5 segundos antes de activar el evento...")  # Indicamos que vamos a esperar 5 segundos
+    # Indicamos que vamos a esperar 5 segundos
+    print("Esperando 5 segundos antes de activar el evento...")
     time.sleep(5)  # Pausamos la ejecución durante 5 segundos
     evento.set()  # Activamos el evento
-    print("El evento ha sido activado después de 5 segundos\n")  # Mensaje indicando que el evento ha sido activado
+    # Mensaje indicando que el evento ha sido activado
+    print("El evento ha sido activado después de 5 segundos\n")
 
 # Creamos dos hilos que ejecutarán las funciones
-hilo1 = threading.Thread(target=esperar_evento)  # Primer hilo ejecutará la función 'esperar_evento'
-hilo2 = threading.Thread(target=activar_evento)  # Segundo hilo ejecutará la función 'activar_evento'
+# Primer hilo ejecutará la función 'esperar_evento'
+hilo1 = threading.Thread(target=esperar_evento)
+# Segundo hilo ejecutará la función 'activar_evento'
+hilo2 = threading.Thread(target=activar_evento)
 
 # Iniciamos los hilos
 hilo1.start()  # Iniciamos el primer hilo
@@ -29,4 +35,5 @@ hilo2.start()  # Iniciamos el segundo hilo
 hilo1.join()  # Espera a que el primer hilo termine
 hilo2.join()  # Espera a que el segundo hilo termine
 
-print("Programa terminado")  # Mensaje indicando que el programa ha finalizado
+# Mensaje indicando que el programa ha finalizado
+print("Programa terminado")
